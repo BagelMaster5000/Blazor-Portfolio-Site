dd121eadd2971780a1318146d121270e711ffccf
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2022-12-17 18:44:57 -0600

    Setup basic main layout and page transitions

---
+++ Blazor Portfolio Site/GlobalVariables.cs
@@ -0,0 +1,3 @@
+﻿public static class GlobalVariables {
+	public static string CurPageName { get; set; } = "Home";
+}
---
+++ Blazor Portfolio Site/Shared/MainLayout.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Drawing;
+using System.Text;
+
+namespace Blazor_Portfolio_Site.Shared;
+
+public partial class MainLayout {
+	public string CurPageClassName {
+		get {
+			switch (GlobalVariables.CurPageName) {
+				case "Home": return "home-class";
+				case "Games": return "games-class";
+				default: return "base-class";
+			}
+		}
+	}
+}
