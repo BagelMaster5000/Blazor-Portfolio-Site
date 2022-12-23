576e88bf7b7133eee922870f618dc06c7c383264
Sammy Mahmoudi <>
2022-12-22 22:42:35 -0500

    Fixed padding on tabs

--- Blazor Portfolio Site/Shared/MainLayout.razor.cs
+++ Blazor Portfolio Site/Shared/MainLayout.razor.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Mvc.Razor;
-using System.Drawing;
-using System.Text;
-
-namespace Blazor_Portfolio_Site.Shared;
+﻿namespace Blazor_Portfolio_Site.Shared;
 
 public partial class MainLayout {
 	public string CurPageClassName {
