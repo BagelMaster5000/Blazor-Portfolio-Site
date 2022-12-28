d44b75ca6ee918d9903a0aed7a7a2d442a4f665a
Sammy Mahmoudi <>
2022-12-28 15:18:05 -0500

    Fixed tabs now update as-expected when hyperlinking to a page

--- Blazor Portfolio Site/GlobalVariables.cs
+++ Blazor Portfolio Site/GlobalVariables.cs
@@ -1,16 +1,16 @@
 ﻿using Blazorise.Animate;
 
 public static class GlobalVariables {
-	public static string CurPageName { get; set; } = "Home";
+    public static int PageAnimationSlideDirection = 0; // 1 if right, -1 if left
+    public static IAnimation PageAnimation {
+        get {
+            switch (PageAnimationSlideDirection) {
+                case > 0: return Animations.FadeLeft;
+                case < 0: return Animations.FadeRight;
+                default: return Animations.FadeIn;
+            }
+        }
+    }
 
-	public static int PageAnimationSlideDirection = 0; // 1 if right, -1 if left
-	public static IAnimation PageAnimation {
-		get {
-			switch (GlobalVariables.PageAnimationSlideDirection) {
-				case > 0: return Animations.FadeLeft;
-				case < 0: return Animations.FadeRight;
-				default: return Animations.FadeIn;
-			}
-		}
-	}
+    public static Action? OnPageJump;
 }
