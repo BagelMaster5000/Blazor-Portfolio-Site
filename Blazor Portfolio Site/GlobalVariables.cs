e9f2729f7d20eabacf28733594dced4086ef1d5e
Sammy Mahmoudi <>
2022-12-27 20:39:39 -0500

    Added animations for page transitions and clicking cards

--- Blazor Portfolio Site/GlobalVariables.cs
+++ Blazor Portfolio Site/GlobalVariables.cs
@@ -1,3 +1,16 @@
-﻿public static class GlobalVariables {
+﻿using Blazorise.Animate;
+
+public static class GlobalVariables {
 	public static string CurPageName { get; set; } = "Home";
+
+	public static int PageAnimationSlideDirection = 0; // 1 if right, -1 if left
+	public static IAnimation PageAnimation {
+		get {
+			switch (GlobalVariables.PageAnimationSlideDirection) {
+				case > 0: return Animations.FadeLeft;
+				case < 0: return Animations.FadeRight;
+				default: return Animations.FadeIn;
+			}
+		}
+	}
 }
