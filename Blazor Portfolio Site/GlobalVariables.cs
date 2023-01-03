b14d6d8348e93e47397d786c1cb54cf8d20eac0f
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2023-01-03 16:48:16 -0500

    Added dark mode

--- Blazor Portfolio Site/GlobalVariables.cs
+++ Blazor Portfolio Site/GlobalVariables.cs
@@ -1,16 +1,48 @@
-﻿using Blazorise.Animate;
+﻿using Blazorise;
+using Blazorise.Animate;
 
 public static class GlobalVariables {
-    public static int PageAnimationSlideDirection = 0; // 1 if right, -1 if left
-    public static IAnimation PageAnimation {
-        get {
-            switch (PageAnimationSlideDirection) {
-                case > 0: return Animations.FadeLeft;
-                case < 0: return Animations.FadeRight;
-                default: return Animations.FadeIn;
-            }
-        }
-    }
+	public static Action? OnPageJump;
+	public static int PageAnimationSlideDirection = 0; // 1 if right, -1 if left
+	public static IAnimation PageAnimation {
+		get {
+			switch (PageAnimationSlideDirection) {
+				case > 0: return Animations.FadeLeft;
+				case < 0: return Animations.FadeRight;
+				default: return Animations.FadeIn;
+			}
+		}
+	}
 
-    public static Action? OnPageJump;
+	// Theme switching
+	public static bool DarkModeActivated = false;
+	public static Theme CurTheme => (DarkModeActivated) ? darkTheme : lightTheme;
+	private static Theme lightTheme = new Theme {
+		TextColorOptions = new ThemeTextColorOptions() {
+			Primary = "#000000",
+		},
+		ColorOptions = new ThemeColorOptions() {
+			Primary = "#ffffff", // Main background color
+			Secondary = "#212121", // Main text color
+			Light = "#ffffff", // Darker background color
+			Info = "#424246", // Darker text color
+		},
+		BackgroundOptions = new ThemeBackgroundOptions() {
+			Primary = "#ffffff", // Main background color
+		},
+	};
+	private static Theme darkTheme = new Theme {
+		TextColorOptions = new ThemeTextColorOptions() {
+			Primary = "#ffffff",
+		},
+		ColorOptions = new ThemeColorOptions() {
+			Primary = "#212121", // Main background color
+			Secondary = "#f1f1f1", // Main text color
+			Light = "#424246", // Lighter background color
+			Info = "#d4d9dd", // Lighter text color
+		},
+		BackgroundOptions = new ThemeBackgroundOptions() {
+			Primary = "#212121", // Main background color
+		},
+	};
 }
